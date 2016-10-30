86fcc942bb8fb2ea3389bf22e0a5c2a8286dbdbc
takamitsu-iida <takamitsu.iida@gmail.com>
2016-10-30 16:59:04 +0900

    20161030 add d3iida.siteTable in d3iida.mapChart.js

--- static/site/js/d3iida.mapChart.js
+++ static/site/js/d3iida.mapChart.js
@@ -14,8 +14,8 @@
     var svg;
 
     // SVGの枠の大きさ
-    var width = 800;
-    var height = 600;
+    var width = 600;
+    var height = 400;
 
     // 'g'の描画領域となるデフォルトのマージン
     var margin = {
@@ -36,12 +36,10 @@
     // 拠点の'circle'の半径
     var siteRadius = 4.0;
 
-    // 地図の拡大・縮小のスケール指定
-    // scaleExtentの最小値
-    var scaleSize = 40;
-
-    // 地図の中心点
-    var center = [139.0032936, 38.5139088];
+    // 地図の縮尺
+    // 小さいほど広域が表示される
+    // 画面サイズと合わせて調整が必要で、経験則的に決める必要がある
+    var scaleSize = 35;
 
     // 日本地図のtopojsonデータ
     var geodata = d3iida.geodata.japan;
@@ -55,9 +53,11 @@
       .translate([0, 0]);
       // .center(center) // center()は最初にcall()するときに決定するので指定しない
 
-    // Compute the projected initial center.
+    // 地図の中心点
+    var center = [139.0032936, 38.5139088];
+
+    // projectionで変換した初期センター値
     var centerPoint = projection(center);
-    console.log(centerPoint);
 
     // 地図用のパスジェネレータ
     var geoPath = d3.geoPath().projection(projection);
@@ -123,9 +123,6 @@
         .attr('width', w).attr('height', h)
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
         .classed('brushLayer', true)
-        .attr('debug', function() {
-          console.log('brushLayer created');
-        })
         .call(d3.brush().extent([[0, 0], [w, h]]).on('brush', function() {
           if (!d3.event.sourceEvent) return; // Only transition after input.
           if (!d3.event.selection) return; // Ignore empty selections.
@@ -152,7 +149,6 @@
       .selectedIndex(0)
       .on('customClick', function(d) {
         var selectedIndex = d;
-        console.log(selectedIndex);
         if (selectedIndex === 0) {
           if (isBrushEnabled) {
             toggleBrush();
@@ -179,9 +175,6 @@
           .attr('width', width).attr('height', height)
           .attr('preserveAspectRatio', 'xMinYMin meet')
           .attr('viewBox', '0 0 ' + width + ' ' + height)
-          .attr('debug', function() {
-            console.log('new svg created');
-          })
           .style('overflow', 'hidden')
           .classed('svg-content-responsive', true);
 
@@ -322,11 +315,18 @@
 
   // 使い方  <div id='mapChart'></div>内に地図を描画する
   d3iida.mapChart.example = function() {
-    // コンテナは<div id='mapChart'>を使う
-    var container = d3.select('#mapChart');
+    // <div id='mapChart'></div>
+    var mapContainer = d3.select('#mapChart');
+    var mapContainerWidth = mapContainer.node().clientWidth;
+
+    // <div id='siteTable'></div>
+    var tableContainer = d3.select('#siteTable');
 
     // 県庁所在地のデータ
     var prefGovLoc = d3iida.geodata.prefectureGovernmentLocationMap;
+
+    // 拠点をテーブル表示するモジュールsiteTableをインスタンス化する
+    var table = d3iida.siteTable();
 
     // crossfilterのディメンジョン定義
     var siteLocation = crossfilter().add(prefGovLoc.features).dimension(function(d) {
@@ -334,9 +334,9 @@
     });
 
     // mapChartをインスタンス化する
-    var mapChart = d3iida.mapChart().width(600).height(600);
+    var mapChart = d3iida.mapChart().width(mapContainerWidth).height(480);
 
-    // ブラシイベントを拾って、境界に収まる拠点の配列を得る
+    // ブラシイベントを拾って、境界に収まる拠点の配列を計算する
     mapChart.on('brushing', function(extents) {
       // 境界ボックスからlongitudeの配列にする
       var longitudes = [extents[0][0], extents[1][0]];
@@ -349,14 +349,15 @@
         return d[0] >= longitudes[0] && d[0] <= longitudes[1] && d[1] >= latitudes[1] && d[1] <= latitudes[0];
       });
 
-      // 境界ボックスに収まる全レコード返す
-      var filtered = siteLocation.top(Infinity);
+      // 境界ボックスに収まる全レコードを得る
+      var filteredLocations = siteLocation.top(Infinity);
 
-      console.log(filtered);
+      // 新しいデータを紐付けてテーブルをcall()する
+      tableContainer.datum(filteredLocations).call(table);
     });
 
-    // コンテナにデータを紐付けてcall()する
-    container.datum(prefGovLoc).call(mapChart);
+    // コンテナに県庁所在地データを紐付けてcall()する
+    mapContainer.datum(prefGovLoc).call(mapChart);
    //
   };
   //
@@ -365,230 +366,269 @@
 //
 //
 //
-
 // radioButtonモジュール
-d3iida.radioButton = function module() {
-  // 領域のサイズ
-  var width = 400;
-  var height = 50;
+(function() {
+  d3iida.radioButton = function module() {
+    // 領域のサイズ
+    var width = 300;
+    var height = 50;
 
-  // 描画領域のマージン
-  var margin = {top: 5, right: 20, bottom: 0, left: 40};
+    // 描画領域のマージン
+    var margin = {top: 5, right: 10, bottom: 0, left: 10};
 
-  // 描画領域のサイズw, h
-  var w = width - margin.left - margin.right;
-  var h = height - margin.top - margin.bottom;
+    // 描画領域のサイズw, h
+    var w = width - margin.left - margin.right;
+    var h = height - margin.top - margin.bottom;
 
-  // ボタンのサイズ、色、配置間隔
-  var buttonSize = 8;
-  var buttonColorOut = '#999';
-  var buttonColorOver = '#333';
-  var buttonColorSelected = '#333';
-  var buttonIntervalWidth = 80;
+    // ボタンのサイズ、色、配置間隔
+    var buttonSize = 8;
+    var buttonColorOut = '#999';
+    var buttonColorOver = '#333';
+    var buttonColorSelected = '#333';
+    var buttonIntervalWidth = 80;
 
-  // タイトル
-  var title = 'ラジオボタン';
-  var titleOffset = 120;
+    // タイトル
+    var title = 'ラジオボタン';
+    var titleOffset = 120;
 
-  // ラベルのフォントサイズ
-  var fontSize = 12;
+    // ラベルのフォントサイズ
+    var fontSize = 12;
 
-  // 選択中のラジオボタンのインデックス
-  var selectedIndex = 0;
+    // 選択中のラジオボタンのインデックス
+    var selectedIndex = 0;
 
-  // 外部にイベントを公開できるようにするためにd3.dispatchを使う。
-  var dispatch = d3.dispatch('customClick', 'stateChange');
+    // 外部にイベントを公開できるようにするためにd3.dispatchを使う。
+    var dispatch = d3.dispatch('customClick', 'stateChange');
 
-  // d3でデータを紐付けしたあとcall()することでこれが呼ばれる
-  function exports(_selection) {
-    _selection.each(function(_data) {
-      // 受け取ったデータを紐付けたSVGを作ることで、SVGの重複作成を防ぐ
-      var svgAll = d3.select(this).selectAll('svg').data([_data]);
+    // d3でデータを紐付けしたあとcall()することでこれが呼ばれる
+    function exports(_selection) {
+      _selection.each(function(_data) {
+        // 受け取ったデータを紐付けたSVGを作ることで、SVGの重複作成を防ぐ
+        var svgAll = d3.select(this).selectAll('svg').data([_data]);
 
-      // ENTER領域
-      var mainG = svgAll.enter()
-        .append('svg')
-        .attr('width', width).attr('height', height)
-        .append('g')
-        .classed('d3iida-radio-main', true)
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        // ENTER領域
+        var mainG = svgAll.enter()
+          .append('svg')
+          .attr('width', width).attr('height', height)
+          .append('g')
+          .classed('d3iida-radio-main', true)
+          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-      // 枠の表示
-      mainG.append('rect')
-        .attr('width', w).attr('height', h)
-        .style('fill', 'none')
-        .style('stroke', '#CCC')
-        .style('stroke-width', 1);
+        // 枠の表示
+        mainG.append('rect')
+          .attr('width', w).attr('height', h)
+          .style('fill', 'none')
+          .style('stroke', '#CCC')
+          .style('stroke-width', 1);
 
-      // タイトル表示
-      mainG.append('text')
-        .attr('x', 10)
-        .attr('y', buttonSize * 3)
-        .attr('fill', buttonColorOut)
-        .attr('font-size', fontSize)
-        .text(title);
+        // タイトル表示
+        mainG.append('text')
+          .attr('x', 10)
+          .attr('y', buttonSize * 3)
+          .attr('fill', buttonColorOut)
+          .attr('font-size', fontSize)
+          .text(title);
 
-      var chartG = mainG.append('g').classed('d3iida-radio-chart', true);
-      var focusG = mainG.append('g').classed('d3iida-radio-focus', true);
-      var labelG = mainG.append('g').classed('d3iida-radio-label', true);
+        var chartG = mainG.append('g').classed('d3iida-radio-chart', true);
+        var focusG = mainG.append('g').classed('d3iida-radio-focus', true);
+        var labelG = mainG.append('g').classed('d3iida-radio-label', true);
 
-      // ラジオボタンをchartGに追加
-      var buttonAll = chartG.selectAll('.d3iida-radio-chart-button').data(_data);
-      // ENTER領域
-      var buttons = buttonAll.enter()
-        .append('circle')
-        .classed('d3iida-radio-chart-button', true)
-        .attr('id', function(d) {
-          return d.index || d.value;
-        })
-        .attr('cx', function(d, i) {
-          return titleOffset + i * buttonIntervalWidth;
-        })
-        .attr('cy', buttonSize * 2)
-        .attr('r', buttonSize)
-        .attr('stroke', 'none')
-        .style('fill', function(d, i) {
-          if (selectedIndex === i) {
+        // ラジオボタンをchartGに追加
+        var buttonAll = chartG.selectAll('.d3iida-radio-chart-button').data(_data);
+        // ENTER領域
+        var buttons = buttonAll.enter()
+          .append('circle')
+          .classed('d3iida-radio-chart-button', true)
+          .attr('id', function(d) {
+            return d.index || d.value;
+          })
+          .attr('cx', function(d, i) {
+            return titleOffset + i * buttonIntervalWidth;
+          })
+          .attr('cy', buttonSize * 2)
+          .attr('r', buttonSize)
+          .attr('stroke', 'none')
+          .style('fill', function(d, i) {
+            if (selectedIndex === i) {
+              d3.select(this).attr('fill', buttonColorSelected);
+            } else {
+              d3.select(this).attr('fill', buttonColorOut);
+            }
+          });
+
+        // ラジオボタンにイベントハンドラを追加
+        buttons
+          .on('mouseover', function() {
+            d3.select(this).attr('fill', buttonColorOver);
+          })
+          .on('mouseout', function(d, i) {
+            if (selectedIndex === i) {
+              d3.select(this).attr('fill', buttonColorSelected);
+            } else {
+              d3.select(this).attr('fill', buttonColorOut);
+            }
+          })
+          .on('click', function(d, i) {
+            selectedIndex = d.index || i;
+            var clicked = d3.select(this);
+            chartG.selectAll('.d3iida-radio-chart-button').attr('fill', buttonColorOut);
             d3.select(this).attr('fill', buttonColorSelected);
-          } else {
-            d3.select(this).attr('fill', buttonColorOut);
-          }
-        });
+            d3.select('#focus').transition().attr('cx', clicked.attr('cx'));
+            // カスタムイベントをディスパッチする
+            dispatch.call('customClick', this, selectedIndex);
+          });
 
-      // ラジオボタンにイベントハンドラを追加
-      buttons
-        .on('mouseover', function() {
-          d3.select(this).attr('fill', buttonColorOver);
-        })
-        .on('mouseout', function(d, i) {
-          if (selectedIndex === i) {
-            d3.select(this).attr('fill', buttonColorSelected);
-          } else {
-            d3.select(this).attr('fill', buttonColorOut);
-          }
-        })
-        .on('click', function(d, i) {
-          selectedIndex = d.index || i;
-          var clicked = d3.select(this);
-          chartG.selectAll('.d3iida-radio-chart-button').attr('fill', buttonColorOut);
-          d3.select(this).attr('fill', buttonColorSelected);
-          d3.select('#focus').transition().attr('cx', clicked.attr('cx'));
-          // カスタムイベントをディスパッチする
-          dispatch.call('customClick', this, selectedIndex);
-        });
+        // ラベル
+        var labelAll = labelG.selectAll('.d3iida-radio-label-text').data(_data);
+        // ENTER領域
+        labelAll.enter()
+          .append('text')
+          .attr('class', 'd3iida-radio-label-text')
+          .attr('x', function(d, i) {
+            return titleOffset + i * buttonIntervalWidth;
+          })
+          .attr('y', buttonSize * 5)
+          .attr('fill', buttonColorOut)
+          .attr('text-anchor', 'middle')
+          .attr('font-size', fontSize)
+          .attr('pointerEvents', 'none')
+          .text(function(d) {
+            return d.label;
+          });
 
-      // ラベル
-      var labelAll = labelG.selectAll('.d3iida-radio-label-text').data(_data);
-      // ENTER領域
-      labelAll.enter()
-        .append('text')
-        .attr('class', 'd3iida-radio-label-text')
-        .attr('x', function(d, i) {
-          return titleOffset + i * buttonIntervalWidth;
-        })
-        .attr('y', buttonSize * 5)
-        .attr('fill', buttonColorOut)
-        .attr('text-anchor', 'middle')
-        .attr('font-size', fontSize)
-        .attr('pointerEvents', 'none')
-        .text(function(d) {
-          return d.label;
-        });
-
-      // focus
-      var focusAll = focusG.selectAll('.d3iida-radio-focus-circle').data([0]);
-      // ENTER領域
-      focusAll.enter()
-        .append('circle')
-        .attr('class', 'd3iida-radio-focus-circle')
-        .attr('id', 'focus')
-        .attr('cx', titleOffset + selectedIndex * buttonIntervalWidth)
-        .attr('cy', buttonSize * 2)
-        .attr('r', buttonSize + 2)
-        .attr('fill', 'none')
-        .attr('stroke-width', 2)
-        .attr('stroke', buttonColorSelected);
-    });
-    //
-  }
-
-  exports.width = function(_) {
-    if (!arguments.length) {
-      return width;
+        // focus
+        var focusAll = focusG.selectAll('.d3iida-radio-focus-circle').data([0]);
+        // ENTER領域
+        focusAll.enter()
+          .append('circle')
+          .attr('class', 'd3iida-radio-focus-circle')
+          .attr('id', 'focus')
+          .attr('cx', titleOffset + selectedIndex * buttonIntervalWidth)
+          .attr('cy', buttonSize * 2)
+          .attr('r', buttonSize + 2)
+          .attr('fill', 'none')
+          .attr('stroke-width', 2)
+          .attr('stroke', buttonColorSelected);
+      });
+      //
     }
-    width = _;
-    return this;
-  };
 
-  exports.height = function(_) {
-    if (!arguments.length) {
-      return height;
+    exports.width = function(_) {
+      if (!arguments.length) {
+        return width;
+      }
+      width = _;
+      return this;
+    };
+
+    exports.height = function(_) {
+      if (!arguments.length) {
+        return height;
+      }
+      height = _;
+      return this;
+    };
+
+    exports.marginLeft = function(_) {
+      if (!arguments.length) {
+        return margin.left;
+      }
+      margin.left = _;
+      return this;
+    };
+
+    exports.marginRight = function(_) {
+      if (!arguments.length) {
+        return margin.right;
+      }
+      margin.right = _;
+      return this;
+    };
+
+    exports.title = function(_) {
+      if (!arguments.length) {
+        return title;
+      }
+      title = _;
+      return this;
+    };
+
+    exports.fontSize = function(_) {
+      if (!arguments.length) {
+        return fontSize;
+      }
+      fontSize = _;
+      return this;
+    };
+
+    exports.selectedIndex = function(_) {
+      if (!arguments.length) {
+        return selectedIndex;
+      }
+      selectedIndex = _;
+      return this;
+    };
+
+    exports.buttonIntervalWidth = function(_) {
+      if (!arguments.length) {
+        return buttonIntervalWidth;
+      }
+      buttonIntervalWidth = _;
+      return this;
+    };
+
+    exports.buttonSize = function(_) {
+      if (!arguments.length) {
+        return buttonSize;
+      }
+      buttonSize = _;
+      return this;
+    };
+
+    exports.on = function() {
+      var value = dispatch.on.apply(dispatch, arguments);
+      return value === dispatch ? exports : value;
+    };
+
+    // 呼び出し元にはexportsを返却する。
+    return exports;
+  };
+})();
+
+//
+//
+//
+// siteTableモジュール
+(function() {
+  d3iida.siteTable = function module() {
+    function exports(_selection) {
+      _selection.each(function(_data) {
+        // <table>を作りなおす
+        d3.select(this).select('table').remove();
+
+        var table = d3.select(this).append('table');
+        var tbody = table.append('tbody');
+
+        // 行を作成
+        tbody.selectAll('tr')
+          .data(_data)
+          .enter()
+          .append('tr') // trタグ追加
+          .selectAll('td')
+          .data(function(row) {
+            // 表示したい情報だけを返す
+            var p = row.properties;
+            // console.log(p);
+            return [p.city];
+          })
+          .enter()
+          .append('td') // tdタグ追加
+          .text(function(d) {
+            return d; // d.value;
+          });
+      });
     }
-    height = _;
-    return this;
-  };
 
-  exports.marginLeft = function(_) {
-    if (!arguments.length) {
-      return margin.left;
-    }
-    margin.left = _;
-    return this;
+    return exports;
   };
-
-  exports.marginRight = function(_) {
-    if (!arguments.length) {
-      return margin.right;
-    }
-    margin.right = _;
-    return this;
-  };
-
-  exports.title = function(_) {
-    if (!arguments.length) {
-      return title;
-    }
-    title = _;
-    return this;
-  };
-
-  exports.fontSize = function(_) {
-    if (!arguments.length) {
-      return fontSize;
-    }
-    fontSize = _;
-    return this;
-  };
-
-  exports.selectedIndex = function(_) {
-    if (!arguments.length) {
-      return selectedIndex;
-    }
-    selectedIndex = _;
-    return this;
-  };
-
-  exports.buttonIntervalWidth = function(_) {
-    if (!arguments.length) {
-      return buttonIntervalWidth;
-    }
-    buttonIntervalWidth = _;
-    return this;
-  };
-
-  exports.buttonSize = function(_) {
-    if (!arguments.length) {
-      return buttonSize;
-    }
-    buttonSize = _;
-    return this;
-  };
-
-  exports.on = function() {
-    var value = dispatch.on.apply(dispatch, arguments);
-    return value === dispatch ? exports : value;
-  };
-
-  // 呼び出し元にはexportsを返却する。
-  return exports;
-};
+})();
